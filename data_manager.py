19389b29aa2a98b74c8e44c2392df5353ddb232e
Harnet69 <harnet69@gmail.com>
2019-12-08 12:42:16 +0100

    Added mentors and schools page

--- data_manager.py
+++ data_manager.py
@@ -11,11 +11,13 @@ def get_mentor_names_by_first_name(cursor, first_name):
     names = cursor.fetchall()
     return names
 
+
 @database_common.connection_handler
 def get_mentors_names_lastnames(cursor):
     cursor.execute("SELECT first_name, last_name FROM mentors;")
     names = cursor.fetchall()
     return names
+
 
 @database_common.connection_handler
 def miskolc_mentors_nicknames(cursor):
@@ -23,22 +25,28 @@ def miskolc_mentors_nicknames(cursor):
     miskolc_mentors_nicknames = cursor.fetchall()
     return miskolc_mentors_nicknames
 
+
 @database_common.connection_handler
 def carols_fullname_number(cursor):
     cursor.execute("SELECT first_name, last_name, phone_number FROM applicants WHERE first_name = 'Carol';")
     carols_fullname_number = cursor.fetchall()
     return carols_fullname_number
 
+
 @database_common.connection_handler
 def another_girls_fullname_number(cursor):
-    cursor.execute("SELECT first_name, last_name, phone_number FROM applicants WHERE email LIKE '%@adipiscingenimmi.edu';")
+    cursor.execute(
+        "SELECT first_name, last_name, phone_number FROM applicants WHERE email LIKE '%@adipiscingenimmi.edu';")
     another_girls_fullname_number = cursor.fetchall()
     return another_girls_fullname_number
+
 
 @database_common.connection_handler
 def add_new_applicant(cursor, first_name, last_name, phone_number, email, application_code):
     try:
-        cursor.execute("INSERT INTO applicants(first_name, last_name, phone_number, email, application_code) VALUES(%s, %s, %s, %s, %s)", (first_name, last_name, phone_number, email, application_code))
+        cursor.execute(
+            "INSERT INTO applicants(first_name, last_name, phone_number, email, application_code) VALUES(%s, %s, %s, %s, %s)",
+            (first_name, last_name, phone_number, email, application_code))
         return True
     except Exception:
         print("Something wrong with add_new_applicant")
@@ -59,7 +67,7 @@ def get_applicants_info(cursor):
     try:
         cursor.execute("SELECT * FROM applicants ORDER BY id DESC")
         applicants_info = cursor.fetchall()
-        return  applicants_info
+        return applicants_info
     except Exception:
         print("Something wrong with get_applicants_info")
 
@@ -73,14 +81,17 @@ def get_applicant_info_by_id(cursor, id):
     except Exception:
         print("Something wrong with get_applicant_info_by_id")
 
+
 @database_common.connection_handler
 def update_applicant_information(cursor, id, first_name, last_name, phone_number, email, application_code):
     try:
-        cursor.execute("UPDATE applicants SET first_name = %s, last_name = %s, phone_number = %s, email = %s, application_code = %s WHERE id = %s",
-                       (id, first_name, last_name, phone_number, email, application_code))
+        cursor.execute(
+            "UPDATE applicants SET first_name = %s, last_name = %s, phone_number = %s, email = %s, application_code = %s WHERE id = %s",
+            (id, first_name, last_name, phone_number, email, application_code))
         return True
     except Exception:
         print("Something wrong with update_applicant_information")
+
 
 @database_common.connection_handler
 def delete_applicant(cursor, app_id):
@@ -89,3 +100,10 @@ def delete_applicant(cursor, app_id):
         return True
     except Exception:
         print("Something wrong with delete_applicant")
+
+
+@database_common.connection_handler
+def mentors_and_schools_info(cursor):
+    cursor.execute("SELECT mentors.first_name as mentors_name, mentors.last_name as mentors_lastname, schools.name as schools_name, schools.country as schools_country FROM mentors INNER JOIN schools ON mentors.id = schools.contact_person")
+    names = cursor.fetchall()
+    return names
--- main.py
+++ main.py
@@ -8,12 +8,13 @@ MENU_PAGES = {'mentors_names_lastnames': ('mentors_names_lastnames.html', 'Mento
               'miskolc_mentors_nicknames': ('miskolc_mentors_nicknames.html', 'Miskolc mentors nicknames'),
               'carols_fullname_number': ('carols_fullname_number.html', "Carol's full name and phone nubmer"),
               'another_girls_fullname_number': (
-              'another_girls__fullname_number.html', "Another girl's full name and phone nubmer"),
+                  'another_girls__fullname_number.html', "Another girl's full name and phone nubmer"),
               'add_new_applicant': ('add_new_applicant', 'add_new_applicant.html', 'Add new applicant',),
               'applicant_info': ('applicant_info.html', 'Applicant info'),
               'applicants_info': ('applicants_info', 'applicants_info.html', 'Applicants info'),
               'applicant_delete': ('applicant_delete', 'applicant_delete.html', 'Delete the applicant'),
-              'applicant_update': ('applicant_update', 'applicant_update.html', 'Update info about an applicant')
+              'applicant_update': ('applicant_update', 'applicant_update.html', 'Update info about an applicant'),
+              'mentors': ('mentors', 'mentors.html', 'Mentors and schools page')
               }
 
 
@@ -28,18 +29,18 @@ def index():
 def mentors_names_lastnames():
     mentors_names_lastnames = data_manager.get_mentors_names_lastnames()
 
-    return render_template(MENU_PAGES['mentors_names_lastnames'][0], mentors_names_lastnames=mentors_names_lastnames) \
- \
- \
+    return render_template(MENU_PAGES['mentors'][0], mentors_names_lastnames=mentors_names_lastnames)
+
+
 # 2. Nicknames all mentors working at Miskolc
 @app.route('/miskolc_mentors_nicknames')
 def miskolc_mentors_nicknames():
     miskolc_mentors_nicknames = data_manager.miskolc_mentors_nicknames()
 
     return render_template(MENU_PAGES['miskolc_mentors_nicknames'][0],
-                           miskolc_mentors_nicknames=miskolc_mentors_nicknames) \
- \
- \
+                           miskolc_mentors_nicknames=miskolc_mentors_nicknames)
+
+
 # 3. Carol's fullname and phone number
 @app.route('/carols_fullname_number')
 def carols_fullname_number():
@@ -103,12 +104,21 @@ def delete_applicant(app_id=0):
 @app.route('/applicant_update/<app_id>', methods=['GET', 'POST'])
 def update_applicant(app_id=0):
     if request.method == "POST":
-        data_manager.update_applicant_information(request.form['first_name'],request.form['last_name'], request.form['phone_number'], request.form['email'], request.form['application_code'], request.form['id'])
+        data_manager.update_applicant_information(request.form['first_name'], request.form['last_name'],
+                                                  request.form['phone_number'], request.form['email'],
+                                                  request.form['application_code'], request.form['id'])
         return redirect('applicants_info')
     else:
         applicant_info_by_id = data_manager.get_applicant_info_by_id(app_id)
         return render_template(MENU_PAGES['applicant_update'][1], applicant_info_by_id=applicant_info_by_id,
                                MENU_PAGES=MENU_PAGES)
+
+# 9. Mentors and schools page
+@app.route('/mentors')
+def mentors():
+    mentors_and_schools_info = data_manager.mentors_and_schools_info()
+
+    return render_template(MENU_PAGES['mentors'][1], mentors_and_schools_info=mentors_and_schools_info)
 
 
 if __name__ == '__main__':
